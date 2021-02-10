4fe8980422ed304ece168248d10488ca917b95de
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-10 17:08:14 +1300

    #21 Rewrite class based components to functional components in client

--- client/src/containers/MainRouter.js
+++ client/src/containers/MainRouter.js
@@ -1,31 +1,25 @@
-import React, {Component} from 'react';
-
+import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import AttendancePage from './pages/AttendancePage';
-import MemberPage from "./pages/MemberPage";
 import EventPage from "./pages/EventPage";
+import MemberPage from "./pages/MemberPage";
+import AttendancePage from "./pages/AttendancePage";
 import HomePage from "./pages/HomePage";
 import BackButton from "../components/navigation/BackButton";
 
 
-class MainRouter extends Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <BrowserRouter>
-          <BackButton />
-          <Switch>
-            <Route exact path={"/events/:operation?"} component={EventPage} />
-            <Route path={"/members/:operation?"} component={MemberPage} />
-            <Route path={"/attendance"} component={AttendancePage} />
-            <Route path={""} component={HomePage} />
-          </Switch>
-        </BrowserRouter>
-      </React.Fragment>
-    );
-  }
+export default function MainRouter() {
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <BackButton />
+        <Switch>
+          <Route exact path={"/events/:operation?"} component={EventPage} />
+          <Route path={"/members/:operation?"} component={MemberPage} />
+          <Route path={"/attendance"} component={AttendancePage} />
+          <Route path={""} component={HomePage} />
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
-
-export default MainRouter;
