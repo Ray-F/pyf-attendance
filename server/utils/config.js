aba512011078619d3ad5f8b411dc7b2334986d21
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-10 18:08:56 +1300

    #11 Create global config variables that change based on the environment

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,2 +1,28 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
+// Enum of the different environments
+const Environment = {
+  DEV: "DEV",
+  PROD: "PROD"
+}
+
+// What environment we are currently in
+const ENV = process.env.ENV.toUpperCase()
+
+// Enum of the .env variable relevant for different environments
+const DatabaseUri = {
+  DEV: process.env.MONGODB_URI_DEV,
+  PROD: process.env.MONGODB_URI_PROD
+}
+
+// Database configuration dependent on our current environment
+const DB_URI = (ENV === Environment.DEV) ? DatabaseUri.DEV : DatabaseUri.PROD
+const DB_NAME = (ENV === Environment.DEV) ? "pyf-attendance-dev" : "pyf-attendance"
+
+
+module.exports = {
+  ENV,
+  DB_URI,
+  DB_NAME
+}
