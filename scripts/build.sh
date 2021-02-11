3e0a16631cd4932d87b71dff28f64516f0a34119
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-11 22:55:46 +1300

    #33 Fix build script stopping on failed checkout

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,6 @@
 
 # IMPORTANT: This script should only be run on the server environment!
 
-# Exit immediately with a command fails
-set -e
-
 # Make master branch up to date with master
 git fetch
 git reset origin/master --hard
