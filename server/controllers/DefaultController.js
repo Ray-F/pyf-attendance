c4ee1e87ff3093f9e9a13f51bf508cc7a28f238e
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-11 22:09:56 +1300

    #33 Add better checks for missing payload or signature

--- server/controllers/DefaultController.js
+++ server/controllers/DefaultController.js
@@ -3,18 +3,20 @@ const config = require('../utils/Config')
 
 const deploy = async (req, res, next) => {
   const payload = JSON.stringify(req.body)
+  const signature = req.get('X-Hub-Signature-256')
 
-  if (!payload) {
-    res.status(404).send("Request body empty")
+  if (!payload || !signature) {
+    res.status(404).send("Payload and/or signature missing")
+    return
   }
 
   // Webhook secret
   const secret = process.env.GH_WEBHOOK_SECRET
-  const githubSignature = req.get('X-Hub-Signature-256')
-  const hmac = crypto.createHmac('sha256', secret)
 
+  // Create comparisons to match
+  const hmac = crypto.createHmac('sha256', secret)
   const digest = Buffer.from('sha256=' + hmac.update(payload).digest('hex'), 'utf8')
-  const checksum = Buffer.from(githubSignature, 'utf8')
+  const checksum = Buffer.from(signature, 'utf8')
 
   if (checksum.length === digest.length && crypto.timingSafeEqual(digest, checksum)) {
     res.status(200).send("Successfully issued deployment command")
