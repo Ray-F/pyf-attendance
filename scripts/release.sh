58955fbcffbeb9604019abfb13dfb8adb96fc113
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-11 20:36:59 +1300

    #33 Update release.sh script with colours

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,11 +7,11 @@ set -e
 
 if [[ "$VERSION" == "" ]]
 then
-  printf "[SCRIPT] ERROR: No version argument sent to the script\n"
+  printf "\e[31m[SCRIPT] ERROR: No version argument sent to the script\e[0m\n"
   exit 1
 fi
 
-printf "[SCRIPT] Updating project version to \e[1m$VERSION\e[0m\n"
+printf "\e[33m[SCRIPT] Updating project version to \e[1m$VERSION\e[0m\n"
 
 PACKAGE_VERSION_STR="s/\"version\":.*/\"version\": \"$VERSION\",/"
 
@@ -23,14 +23,14 @@ sed -i "" "$PACKAGE_VERSION_STR" client/package.json
 sed -i "" "$PACKAGE_VERSION_STR" server/package.json
 
 git add .
-git commit -m "[SCRIPT] Update project version to $VERSION"
+git commit -m "[BOT] Update project version to $VERSION"
 git push origin dev
 
-printf "\n[SCRIPT] Tagging new version ($VERSION)...\n"
-git tag -a $VERSION -m ""
+printf "\n\e[33m[SCRIPT] Tagging new version ($VERSION)...\e[0m\n"
+git tag -a "$VERSION" -m ""
 git push --tags
 
-printf "\n[SCRIPT] Merging to master...\n"
+printf "\n\e[33m[SCRIPT] Merging to master...\e[0m\n"
 git checkout master
 git merge dev
 git push origin master
