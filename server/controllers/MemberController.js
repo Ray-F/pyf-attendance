a8ebe57f1cfbb2ca96a7cb4ab91f020f490032bb
Raymond Feng <raymondf.5522@gmail.com>
2021-02-16 22:55:22 +1300

    #31 Restructure MongoRepository to incorporate Event and Member DBO/DTO classes
    * #31 Revamp Attendance and add Member class
    
    * #31 Add Event class and remove static factory methods
    
    * #31 Make JSDoc and other commenting changes
    
    * #22 Update CI test to use node version 14.*
    
    * #22 Fix YML syntax on ci-test.yml

--- server/controllers/MemberController.js
+++ server/controllers/MemberController.js
@@ -1,51 +1,68 @@
 const {
-  getMembersFromDb, saveMemberToDb,
-  deleteMemberFromDb, deleteAllMembersFromDb,
-  deleteAttendanceFromDb, deleteAllAttendanceFromDb
+  getMembersFromDb,
+  getMemberFromDb,
+  saveMemberToDb,
+  deleteMemberFromDb,
+  deleteAllMembersFromDb,
+  deleteAttendanceFromDb,
+  deleteAllAttendanceFromDb
 } = require("../models/mongodb/MongoRepository");
 
+const { Member } = require('../models/Member')
 
+
+/**
+ * Gets all members or a single member (if `req.query.memberId` is specified).
+ *
+ * @param {string} [req.query.memberId] - The ID of the member to get.
+ */
 const getMembers = async (req, res, next) => {
-  if (req.query.memberId !== undefined) {
-    res.json(await getMembersFromDb(req.query.memberId))
-  } else {
-    res.json(await getMembersFromDb())
-  }
+  const members = req.query.memberId ? (await getMemberFromDb(req.query.memberId)).toDto()
+    : (await getMembersFromDb()).map((member) => (member.toDto()));
+  res.json(members)
 }
 
+/**
+ * Saves a member.
+ *
+ * @param {string} [req.body._id] - The ID of the member.
+ * @param {string} req.body.fullName - The name of the member.
+ * @param {string} req.body.startDate - The starting date of the member.
+ * @param {string} [req.body.endDate] - The ending date of the member.
+ */
 const saveMember = async (req, res, next) => {
-  let memberObject = req.body
-
-  memberObject.startDate = new Date(memberObject.startDate)
-
-  if (memberObject.endDate !== undefined) {
-    memberObject.endDate = new Date(memberObject.endDate)
-  }
-
-  await saveMemberToDb(memberObject)
-
+  await saveMemberToDb(new Member(req.body));
   res.sendStatus(200)
 }
 
+/**
+ * Deletes the specified member along with their attendance information.
+ *
+ * @param {string} req.query.memberId - The ID of the member to delete.
+ */
 const deleteMember = async (req, res, next) => {
   let memberId = req.query.memberId
 
-  if (memberId === undefined) {
-    res.status(404).send("No member Id specified! No records deleted").end()
-  } else {
+  if (memberId) {
     const deleteResult = await deleteMemberFromDb(memberId)
     const deleteAttendanceResult = await deleteAttendanceFromDb(null, memberId)
 
     res.status(200).send(`Delete successful: ${deleteResult.result.n} member(s) and ${deleteAttendanceResult.result.n} attendance record(s) were wiped.`)
+  } else {
+    res.status(404).send("No member Id specified! No records deleted").end()
   }
 }
 
+/**
+ * Deletes all members along with their attendance records.
+ */
 const resetMembers = async (req, res, next) => {
   const deleteResult = await deleteAllMembersFromDb()
   const deleteAttendanceResult = await deleteAllAttendanceFromDb()
 
   res.status(200).send(`Delete successful: ${deleteResult.result.n} members and ${deleteAttendanceResult.result.n} attendance records were wiped.`)
 }
+
 
 module.exports = {
   getMembers, saveMember, deleteMember, resetMembers
