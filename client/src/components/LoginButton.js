437e89998ecc05184e8941137a9bcba4681c725c
Raymond Feng <rf.raymondfeng@gmail.com>
2021-03-10 16:43:30 +1300

    #6 Make LoginButton component

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function LoginButton(props) {
+  return (
+    <div>
+      <Button>Login with Google</Button>
+    </div>
+  );
+}
+
+export default LoginButton;
